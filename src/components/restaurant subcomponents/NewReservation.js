8cb0ba1ca3cecec7977682986ff2492b8faa2eb9
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-06-28 01:35:09 -0400

    fixed warnings before merge to main

--- src/components/restaurant subcomponents/NewReservation.js
+++ src/components/restaurant subcomponents/NewReservation.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import {
@@ -30,8 +29,6 @@ export const NewReservation = ({
     setMessage, 
     message
 }) => {
-
-    const navigate = useNavigate();
 
     const [form, setForm] = useState(() => {
         return {
